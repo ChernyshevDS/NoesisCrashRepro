7430e6a0e9f1c03efdb44c1dab77e02ceeeff055
daniilch <daniilch@briogroup.ru>
2023-10-03 09:21:58 +0300

    Added second crash case

--- Assets/Views/EmptyView.xaml.cs
+++ Assets/Views/EmptyView.xaml.cs
@@ -9,6 +9,9 @@ namespace Views
 {
     public partial class EmptyView : UserControl
     {
+        private Visual rootVisual;
+        private Border border;
+
         public EmptyView()
         {
             InitializeComponent();
@@ -20,5 +23,43 @@ namespace Views
             NoesisUnity.LoadComponent(this);
         }
 #endif
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            rootVisual = FindRootVisual();
+            border = (Border)this.GetTemplateChild("border");
+
+            border.MouseLeftButtonDown += EmptyView_MouseLeftButtonDown;
+        }
+
+        private void EmptyView_MouseLeftButtonDown(object sender, MouseButtonEventArgs args)
+        {
+            var screenPos = args.GetPosition(null);
+            var rootVisualPos = rootVisual.PointFromScreen(screenPos);
+            
+            HitTestResult result = null;
+
+            VisualTreeHelper.HitTest(rootVisual,
+                         target =>
+                         {
+                             var uiElement = target as UIElement;
+                             if ((uiElement != null) && (!uiElement.IsHitTestVisible || !uiElement.IsVisible))
+                                 return HitTestFilterBehavior.ContinueSkipSelfAndChildren;
+                             else
+                                 return HitTestFilterBehavior.Continue;
+                         },
+                         target =>
+                         {
+                             result = target;
+                             return HitTestResultBehavior.Stop;
+                         },
+                         new PointHitTestParameters(rootVisualPos));
+
+            UnityEngine.Debug.Log(result?.VisualHit?.ToString() ?? "<no hit>");
+        }
+
+        private Visual FindRootVisual() => (Visual)VisualTreeHelper.GetRoot(this);
     }
 }
